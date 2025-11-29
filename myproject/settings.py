bd062a16eb25779314cec64949a5b2d815e30b2f
gzzhang5 <gzzhang5@gmail.com>
2025-11-28 21:17:38 -0800

    fix imports in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from socket import gethostname, gethostbyname, gethostbyname_ex
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
