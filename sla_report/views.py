5078451a13791f94d23c5a2e3142485c2d9f57b5
Grace <gzzhang5@gmail.com>
2025-11-30 08:20:48 +0000

    add sql to views.py

--- sla_report/views.py
+++ sla_report/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from sla_report.models import Sla_Report
 
-def home(request):
-    return HttpResponse("SLA Report")
+class sla_report(TemplateView):
+    template_name = 'sla_report/summary.html'
+    title = 'Summary'
+
+    def data(self):
+        filter = 'hotspot%'
+        hotspot = Sla_Report.objects.raw('SELECT * FROM sla_report WHERE "group" LIKE %s ORDER BY name', [filter])
+        filter1 = 'billing%' 
+        billing = Sla_Report.objects.raw('SELECT * FROM sla_report WHERE "group" LIKE %s ORDER BY name', [filter1])       
+        filter2 = 'subscription%'
+        subscription = Sla_Report.objects.raw('SELECT * FROM sla_report WHERE "group" LIKE %s ORDER BY name', [filter2])
+
+        products = Sla_Report.objects.raw('SELECT * FROM sla_report WHERE "group" LIKE %s ORDER BY name', [filter12])
+ 
+        context = {
+            'hotspot': hotspot,
+            'billing': billing,
+            'subscription': subscription,
+        }
+
+        return context
+
