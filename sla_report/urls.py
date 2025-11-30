fd68c097e2356c66018e7332f49f109f0ad63b58
Grace <gzzhang5@gmail.com>
2025-11-30 09:34:12 +0000

    modify sla_report urls.py

--- sla_report/urls.py
+++ sla_report/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.urls import re_path
+
+from sla_report.views import sla_report
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    re_path(r'', sla_report.as_view()),
 ]
