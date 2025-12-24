65943b0bf5ec7848c5b32e9b6a6d573c4072ca2e
gzzhang5 <gzzhang5@gmail.com>
2025-12-24 12:10:17 -0800

    Modify models

--- hr/models.py
+++ hr/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Employee(models.Model):
-    employee_id = models.IntegerField(max_length=6, unique=True)
+    employee_id = models.PositiveIntegerField(unique=True)
     name = models.CharField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
     
-    access_level = models.CharField(max_length=10)
-    work_status = models.CharField(max_length=10)
+    access_level = models.CharField(max_length=20)
+    work_status = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.employee_id} - {self.name}"
