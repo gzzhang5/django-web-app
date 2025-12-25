fa027531fda0bf09b735c036cede77e0b7898d98
gzzhang5 <gzzhang5@gmail.com>
2025-12-24 18:28:46 -0800

    Modify urls.py

--- hr/urls.py
+++ hr/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employees/', views.EmployeeListView.as_view(), name='employee-list'),
+    path('', views.index, name='hr-home'),
 ]
